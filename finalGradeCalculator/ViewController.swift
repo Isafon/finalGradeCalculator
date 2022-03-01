44c1614b5ef8b73ab0214f60d4ef3f04b7469252
Isa Fontana <isfon8@gmail.com>
2022-03-01 14:26:08 -0700

    Initial Commit

---
+++ finalGradeCalculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  finalGradeCalculator
+//
+//  Created by isa fontana on 3/1/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
