c381aa2fc4bf6ce892e4dc52d2fed7649938e9b5
Isa Fontana <isfon8@gmail.com>
2022-03-01 15:12:53 -0700

    MVP 1

--- finalGradeCalculator/ViewController.swift
+++ finalGradeCalculator/ViewController.swift
@@ -17,18 +17,33 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var answerLabel: UILabel!
     
-    
-    
-    
-    
-    
-    
-    
-    
+
+    @IBAction func calculateButton(_ sender: UIButton) {
+        
+        let currentGradeString = currentGradeTextField.text ?? ""
+        let desiredGradeString = desiredGradeTextField.text ?? ""
+        let examPercentWorthString = examPercentWorthTextField.text ?? ""
+        
+        var input1 = Double(currentGradeString) ?? 0
+        var input2 = Double(desiredGradeString) ?? 0
+        var input3 = Double(examPercentWorthString) ?? 0
+        
+        var calculate = ((input2) - ((1 - (input3/100))*input1))/input3
+        
+        var resultPercent = 100*calculate
+      
+        
+        answerLabel.text = calculate as? String
+        answerLabel.text = "You need a \(resultPercent)% on the exam"
+        
+        
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        answerLabel.text = "Answer"
     }
 
 
